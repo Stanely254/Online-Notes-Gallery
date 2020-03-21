0f42a403efaab96bb5310e7afcc11a23d78076ec
Stanely254 <47238250+Stanely254@users.noreply.github.com>
2020-03-21 14:45:13 +0300

    Update dbh.inc.php

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -1,6 +1,6 @@
 <?php
 
-$conn = mysqli_connect("localhost", "root", "", "online");
+$conn = mysqli_connect("localhost", "your username", "your password", "your database");
 
 if(mysqli_connect_errno()){
   printf("Connection failed: %s\n", mysqli_connect_error());
